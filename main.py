23dd935f0ddcaaa183c67369888f979a032327c1
AbhilashMuthyala <abhilashreddy989@gmail.com>
2021-11-14 09:10:11 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+
+import smtplib, datetime, random
+
+def send_email(msg):
+    my_email = 'abhilashm791@gmail.com'
+    password = '63207549'
+    with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email,password=password)
+        connection.sendmail(from_addr=my_email,
+                        to_addrs="abhi6j5@gmail.com",
+                        msg=f"Subject: Hello \n\n {msg}")
+
+def current_date():
+    cur_time = datetime.datetime.now()
+    if cur_time.weekday() == 6:
+        quote = read_quotes()
+        send_email(quote)
+
+def read_quotes():
+    with open('quotes.txt') as quotes_file:
+        list_quotes = quotes_file.readlines()
+        random_quote = random.choice(list_quotes)
+        return random_quote
+
+if __name__ == '__main__':
+    current_date()
